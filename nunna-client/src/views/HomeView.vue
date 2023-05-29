f0db4d9d5d37a7d58d0fdf48c0a8db971acefb43
GandyA23 <upgrade.dev.gandy@gmail.com>
2023-05-29 00:22:10 -0600

    Add a simple vue project
    Simple Page Application to test in railway.

---
+++ nunna-client/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+      <button class="button">This is a test button</button>
+      <progress class="progress" value="15" max="100">15%</progress>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+
+.home
+  padding: 1em
+
+</style>
